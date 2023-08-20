be9074186786444a5dffc5c5c77707139cc5afe9
sadiaafrin67 <sa540276@gmail.com>
2023-08-20 23:05:12 +0600

    add my banner section

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('hiiii')
+function handleclick(target){
+console.log(target.children[1].children[1].innerText)
+console.log(target.children[1].children[2].children[0].innerText)
+
+
+
+
+    
+    
+
+}
+
